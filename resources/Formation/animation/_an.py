b232adfeaf5dae10160ac6966c8579ddfc49e8e9
odisho1997 <86355012+odisho1997@users.noreply.github.com>
2021-07-14 18:46:40 +0300

    Update _an.py

--- resources/Formation/animation/_an.py
+++ resources/Formation/animation/_an.py
@@ -1,13 +1,6 @@
-#   ICSS - USERBOT
-#   TELE - @NIIIN2
-
-#  ======================================================= #
-
 from userbot.Config import Config
 
 USERID = Config.OWNER_ID 
 ALIVE_NAME = Config.ALIVE_NAME
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ICSS-USERBOT"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "November-USERBOT"
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
-
-#  ======================================================= #
